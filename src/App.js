a9c0f5cf47a7784ac494a2220102ea6acc1cdb6d
Teresa Pelinski <teresapelinski@gmail.com>
2021-12-30 19:11:19 +0100

    added sass

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
+import './App.scss';
 
 export const App = () => {
 
     return (
         <main>
-            this is a react template
+            <h1>this is a react template</h1>
         </main>
     )
 }
